13811e2d5c0a894e45c8b55e2677f51041fc56a3
polargg <luckyjack85@gmail.com>
2017-05-04 22:15:17 +0800

    init

---
+++ javascripts/style.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    var canvas = $(".navclick");
+    // var b = false;
+    canvas.click(function () {
+
+      if(document.getElementById("intro").style.left=="300px") {
+        $(".navlist").hide();
+        document.getElementById("intro").style.left="0";
+        document.getElementById("detail").style.left="0";
+        document.getElementById("nav").style.left="0";
+        document.getElementById("joinfooter").style.left="400px";
+      } else {
+        $(".navlist").fadeIn(1000, "linear");
+        document.getElementById("intro").style.left="300px";
+        document.getElementById("detail").style.left="300px";
+        document.getElementById("nav").style.left="300px";
+        document.getElementById("joinfooter").style.left="700px";
+      }
+    });
+
+});
